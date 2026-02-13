54ac6e881bcaf81c05187de4981c1c00751c51ea
Abel-RV <ramirezvillarejoabel@gmail.com>
2026-02-13 13:48:37 +0100

    nose

---
+++ Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ramirez_villarejo_abel_ReparaMAUI.Models
+{
+    public class Cliente
+    {
+        [PrimaryKey]
+        public int idCliente {  get; set; }
+        public string movil {  get; set; }
+        public string nombreYApellidos { get; set; }
+
+        public List<Coche> Vehiculos { get; set; } = new List<Coche>();
+    }
+}
---
+++ Models/Coche.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace ramirez_villarejo_abel_ReparaMAUI.Models
+{
+    public class Coche
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+
+        public string Propietario { get; set; }
+
+        public bool Anterior2000 { get; set; }
+
+        public string Motivo { get; set; }
+
+        public DateTime FechaTaller { get; set; }
+
+        public DateTime? FechaAlta { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Cliente Cliente { get; set; }
+    }
+}
